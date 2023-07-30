452340ed48232d8794510573aa60ed450f9251e7
SirineZoghlami <sirine.zoghlami@esprit.tn>
2023-07-30 04:49:52 +0100

    offer ride form step 3 of 4

--- front-end/src/components/index.js
+++ front-end/src/components/index.js
@@ -7,5 +7,6 @@ import Navbar from "./Navbar/Navbar";
 import Agenda from "./Agenda/Agenda";
 import RideCard from "./RideCard/RideCard";
 import OfferRideS2 from "./OfferRideS2/OfferRideS2";
+import OfferRideS3 from "./OfferRideS3/OfferRideS3";
 
-export { Header, Footer, Info, Event, Body, Navbar, Agenda, RideCard, OfferRideS2};
+export { Header, Footer, Info, Event, Body, Navbar, Agenda, RideCard, OfferRideS2, OfferRideS3};
