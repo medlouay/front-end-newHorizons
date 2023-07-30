87f033bdbf00263b88f95e27023eed0b7959f6bd
SirineZoghlami <sirine.zoghlami@esprit.tn>
2023-07-30 23:06:38 +0100

    fixes

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,9 +1,5 @@
 import React from 'react'
-<<<<<<< HEAD
-import{Info,Header,Footer,Body,Event,OfferRideS1, OfferRideS2, OfferRideS3, OfferRideS4} from "./components"
-=======
 import{Info,Header,Footer,Body,Event,OfferRideS1, OfferRideS2, OfferRideS3, OfferRideS4,RideSubmitted} from "./components"
->>>>>>> f1b57e0 (offer ride back card)
 import {path as SvgImage} from "./assets"
 
 const App = () => {
@@ -12,7 +8,7 @@ const App = () => {
       <SvgImage className="styled-svg" />
     </div> */}
   
-  <OfferRideS4/>
+  <RideSubmitted/>
 
   
   </>)
--- front-end/src/components/OfferRideS4/OfferRideS4.jsx
+++ front-end/src/components/OfferRideS4/OfferRideS4.jsx
@@ -2,66 +2,15 @@ import React from 'react';
 import './OfferRideS4.css';
 
 const OfferRideS4 = () => {
-<<<<<<< HEAD
-    
-  return (
-    <div id="container4">
-
-
-           <div id='title'>
-=======
   return (
     <div id="container4">
       <div id='title'>
->>>>>>> f1b57e0 (offer ride back card)
         <span>Offer Ride</span>
         {/* Other JSX elements for the rest of the component */}
       </div>
       <div id='st2'>
         <span>STEP 4 OF 4</span>
       </div>
-<<<<<<< HEAD
-
-      <span id='form4'> Number of available seats </span>
-
-      <div>
-      <select id="number-of-available-seats">
-        <option value="1"> 1</option>
-        <option value="2"> 2</option>
-        <option value="3"> 3</option>
-        <option value="4">4</option>
-      </select>
-    </div>
-
-    <span id='price-for-passangers'> Price for passengers </span>
-
-
-<div id="container-price-for-passangers">
-<div>
-      <label htmlFor="inputField"></label>
-      <input id="input-price-for-passanger"type="text" />
-    </div>
-
-   
-    
-</div>
-
-<div>
-      <select id="select-price-for-passenger">
-        <option value="1"> EUR</option>
-        <option value="2"> 2</option>
-        <option value="3"> 3</option>
-        <option value="4">4</option>
-      </select>
-      </div>
-
-
-        </div>
-      );
-      }
-
-export default OfferRideS4;
-=======
       <span id='form4'> Number of available seats </span>
       <div>
         <select id="number-of-available-seats">
@@ -106,4 +55,3 @@ export default OfferRideS4;
 }
 
 export default OfferRideS4;
->>>>>>> f1b57e0 (offer ride back card)
